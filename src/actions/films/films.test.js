8787306ebc9e745ce87526213b66c5ee8cdf052c
Vasya Ponomrenko <777Vasya77@gmail.com>
2019-12-16 12:37:37 +0200

    Добавил тестов на экшны

--- src/actions/films/films.test.js
+++ src/actions/films/films.test.js
@@ -2,6 +2,24 @@ import {actions} from '~/actions/actions';
 import ActionType from '~/actions/films/action-types';
 
 describe(`Films action creator work correctly`, () => {
+  it(`Action creator for set loading return correctly action`, () => {
+    expect(actions.films.setLoading(true)).toEqual({
+      type: ActionType.SET_LOADING,
+      payload: true
+    });
+  });
+
+  it(`Action creator for set error return correctly action`, () => {
+    expect(actions.films.setError({
+      status: 404
+    })).toEqual({
+      type: ActionType.SET_ERROR,
+      payload: {
+        error: {status: 404}
+      }
+    });
+  });
+
   it(`Action creator for set genre filter return correctly action`, () => {
     expect(actions.films.setGenreFilter(`Other genre`)).toEqual({
       type: ActionType.SET_GENRE_FILTER,
@@ -19,6 +37,15 @@ describe(`Films action creator work correctly`, () => {
   it(`Action creator for reset films per page return correctly action`, () => {
     expect(actions.films.resetFilmsPerPage()).toEqual({
       type: ActionType.RESET_FILMS_PER_PAGE
+    });
+  });
+
+  it(`Action creator for load films return correctly action`, () => {
+    expect(actions.films.loadFilms([
+      {film: 1}
+    ])).toEqual({
+      type: ActionType.LOAD_FILMS,
+      payload: [{film: 1}]
     });
   });
 });
