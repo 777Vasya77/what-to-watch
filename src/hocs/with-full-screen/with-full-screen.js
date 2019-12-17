87058e6e7f3fe0c6dcea1e15e5c43d5398d6072d
Vasya Ponomrenko <777Vasya77@gmail.com>
2019-12-17 19:47:32 +0200

    Добавил полноэкранный режим

--- src/hocs/with-full-screen/with-full-screen.js
+++ src/hocs/with-full-screen/with-full-screen.js
@@ -1,6 +1,7 @@
-import React, {Fragment, PureComponent} from 'react';
+import React, {Fragment, PureComponent, createRef} from 'react';
 import PropTypes from 'prop-types';
 import playerTimeService from "~/services/player-time-service/player-time-service";
+import {fullScreenService} from "~/services/full-screen-service/full-screen-service";
 
 const withFullScreen = (Component) => {
   class WithFullScreen extends PureComponent {
@@ -14,9 +15,12 @@ const withFullScreen = (Component) => {
         timeToEnd: 0,
       };
 
+      this._videoWrapRef = createRef();
+
       this._handleChangeIsFullScreen = this._handleChangeIsFullScreen.bind(this);
       this._handlePlayButton = this._handlePlayButton.bind(this);
       this._handleExitButton = this._handleExitButton.bind(this);
+      this._handleFullScreenButton = this._handleFullScreenButton.bind(this);
       this._updateBar = this._updateBar.bind(this);
     }
 
@@ -24,6 +28,7 @@ const withFullScreen = (Component) => {
       const video = evt.target;
       const currentTime = video.currentTime;
       const duration = video.duration;
+
       this.setState({
         barWidth: currentTime / duration * 100,
         timeToEnd: duration - currentTime
@@ -44,11 +49,22 @@ const withFullScreen = (Component) => {
       onFullScreeExit();
     }
 
+    _handleFullScreenButton() {
+      const {isFullScreen} = this.state;
+      if (isFullScreen) {
+        fullScreenService.close();
+        this.setState({isFullScreen: false});
+      } else {
+        fullScreenService.open(this._videoWrapRef.current);
+        this.setState({isFullScreen: true});
+      }
+    }
+
     render() {
       const {isFullScreen, isPlaying, barWidth, timeToEnd} = this.state;
 
       return (
-        <div className="player">
+        <div className="player" ref={this._videoWrapRef}>
           <Component
             {...this.props}
             fullScreenPlayer
@@ -88,7 +104,7 @@ const withFullScreen = (Component) => {
               </button>
               <div className="player__name">Transpotting</div>
 
-              <button type="button" className="player__full-screen">
+              <button type="button" className="player__full-screen" onClick={this._handleFullScreenButton}>
                 <svg viewBox="0 0 27 27" width="27" height="27">
                   <use xlinkHref="#full-screen"/>
                 </svg>
